189f740c296da36251d9370614f3b2cf7c32779b
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2022-03-04 20:08:33 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-apt update && apt install python3.9 -y && apt install python3-pip -y && apt install screen -y && screen -S bot0001 foto.py
+apt install python3.9 -y && apt install python3-pip -y && apt install screen -y && screen -S bot0001 foto.py
